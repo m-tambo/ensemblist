0434adb19e161f12489071b3e16b56269a0e56b1
Michael Tambornino <michael.tambornino@gmail.com>
2017-05-12 14:27:54 -0500

    added capitalization function

--- app/controllers/profileCtrl.js
+++ app/controllers/profileCtrl.js
@@ -1,5 +1,11 @@
-app.controller('profileCtrl', function () {
+app.controller('profileCtrl', function ($scope, user) {
 
   console.log(`firing the profile controller`)
+  String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1)
+  }
+  $scope.first = user.first.capitalize()
+  $scope.last = user.last.capitalize()
+
 
 })
