d7653b8653ad6bbc4d058c3367e90c809e7b218b
Michael Tambornino <michael.tambornino@gmail.com>
2017-05-12 09:46:04 -0500

    routing from landing page to gig input

--- app/controllers/landingCtrl.js
+++ app/controllers/landingCtrl.js
@@ -1,5 +1,9 @@
-app.controller('landingCtrl', function () {
+app.controller('landingCtrl', function ($scope, $location) {
 
   console.log(`firing the landing controller`)
+
+  $scope.goToGigInput = () => {
+    $location.url('/newgig')
+  }
 
 })
