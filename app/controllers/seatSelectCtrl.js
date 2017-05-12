719f430b3f7e0e5568ffec4919e74c2c47d94503
Michael Tambornino <michael.tambornino@gmail.com>
2017-05-12 14:29:20 -0500

    updating each seat via repeated select options

--- app/controllers/seatSelectCtrl.js
+++ app/controllers/seatSelectCtrl.js
@@ -1,9 +1,25 @@
-app.controller('seatSelectCtrl', function ($scope, seats, instruments) {
+app.controller('seatSelectCtrl', function ($scope, seats, instruments, apiFactory, $stateParams, $location) {
+    // current gig_id is saved as $stateParams.id
 
-  console.log(`firing the seat select controller`)
-
+    // declare the resolve objects on scope
   $scope.seats = seats
   $scope.instruments = instruments
 
-  console.log("instruments:", instruments)
+    // each time the option is changed, update the corresponding seat with the new instrument
+  $scope.saveInstrument = (inst, seatId) => {
+    console.log('instrument:', inst, seatId)
+    let updatedSeat = {
+      id: seatId,
+      user_id: null,
+      gig_id: $stateParams.id,
+      instrument: inst,
+      status: null
+    }
+    apiFactory.updateSeat(seatId, updatedSeat)
+  }
+
+  $scope.goToGigView = () => {
+    $location.url(`/gig/${$stateParams.id}`)
+  }
+
 })
