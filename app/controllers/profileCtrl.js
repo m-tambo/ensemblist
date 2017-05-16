b667b6646104da1c53b6fb95a14b607ed7c31738
Michael Tambornino <michael.tambornino@gmail.com>
2017-05-16 00:21:48 -0500

    filling out profile view

--- app/controllers/profileCtrl.js
+++ app/controllers/profileCtrl.js
@@ -4,6 +4,7 @@ app.controller('profileCtrl', function ($scope, user) {
   String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1)
   }
+  $scope.user = user
   $scope.first = user.first.capitalize()
   $scope.last = user.last.capitalize()
 
