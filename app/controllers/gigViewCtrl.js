57318f7704a110b22d2d6718149124967d2b29b2
Michael Tambornino <michael.tambornino@gmail.com>
2017-05-15 20:32:17 -0500

    ditching modals

--- app/controllers/gigViewCtrl.js
+++ app/controllers/gigViewCtrl.js
@@ -1,4 +1,4 @@
-app.controller('gigViewCtrl', function ($scope, $stateParams, gig, seats, apiFactory, $state, $uibModal, $log, $document, $sce) {
+app.controller('gigViewCtrl', function ($scope, $stateParams, gig, seats, apiFactory, $state, $location, $anchorScroll) {
 
   $scope.gig = gig
   $scope.seats = seats
@@ -57,116 +57,116 @@ app.controller('gigViewCtrl', function ($scope, $stateParams, gig, seats, apiFac
   }
 
     //_______UI BOOTSTRAP TOOLTIP/MODAL ATTEMPT________
-  $(document).ready( () => {
+  // $(document).ready( () => {
 
-    // $('[data-toggle=tooltip]').hover( () => {
-    //     // on mouseenter
-    //     $(this).tooltip('show');
-    // }, function(){
-    //     // on mouseleave
-    //     $(this).tooltip('hide');
-    // })
+  //   // $('[data-toggle=tooltip]').hover( () => {
+  //   //     // on mouseenter
+  //   //     $(this).tooltip('show');
+  //   // }, function(){
+  //   //     // on mouseleave
+  //   //     $(this).tooltip('hide');
+  //   // })
 
-    // _______TOOLTIP_______
-    $scope.dynamicTooltip = 'Hello, World!';
-    $scope.dynamicTooltipText = 'dynamic';
-    $scope.htmlTooltip = $sce.trustAsHtml('I\'ve been made <b>bold</b>!');
-    $scope.placement = {
-      options: [
-        'top',
-        'top-left',
-        'top-right',
-        'bottom',
-        'bottom-left',
-        'bottom-right',
-        'left',
-        'left-top',
-        'left-bottom',
-        'right',
-        'right-top',
-        'right-bottom'
-      ],
-      selected: 'top'
-    };  // _______END OF TOOLTIP______
+  //   // _______TOOLTIP_______
+  //   $scope.dynamicTooltip = 'Hello, World!';
+  //   $scope.dynamicTooltipText = 'dynamic';
+  //   $scope.htmlTooltip = $sce.trustAsHtml('I\'ve been made <b>bold</b>!');
+  //   $scope.placement = {
+  //     options: [
+  //       'top',
+  //       'top-left',
+  //       'top-right',
+  //       'bottom',
+  //       'bottom-left',
+  //       'bottom-right',
+  //       'left',
+  //       'left-top',
+  //       'left-bottom',
+  //       'right',
+  //       'right-top',
+  //       'right-bottom'
+  //     ],
+  //     selected: 'top'
+  //   };  // _______END OF TOOLTIP______
 
-    // _______MODAL_______
-    var $ctrl = this;
-    $ctrl.items = ['item1', 'item2', 'item3'];
+  //   // _______MODAL_______
+  //   var $ctrl = this;
+  //   $ctrl.items = ['item1', 'item2', 'item3'];
 
-    $ctrl.animationsEnabled = true;
+  //   $ctrl.animationsEnabled = true;
 
-    $ctrl.open = function (size, parentSelector) {
-      var parentElem = parentSelector ?
-        angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
-      var modalInstance = $uibModal.open({
-        animation: $ctrl.animationsEnabled,
-        ariaLabelledBy: 'modal-title',
-        ariaDescribedBy: 'modal-body',
-        templateUrl: 'myModalContent.html',
-        controller: 'ModalInstanceCtrl',
-        controllerAs: '$ctrl',
-        size: size,
-        appendTo: parentElem,
-        resolve: {
-          items: function () {
-            return $ctrl.items;
-          }
-        }
-      });
+  //   $ctrl.open = function (size, parentSelector) {
+  //     var parentElem = parentSelector ?
+  //       angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
+  //     var modalInstance = $uibModal.open({
+  //       animation: $ctrl.animationsEnabled,
+  //       ariaLabelledBy: 'modal-title',
+  //       ariaDescribedBy: 'modal-body',
+  //       templateUrl: 'myModalContent.html',
+  //       controller: 'ModalInstanceCtrl',
+  //       controllerAs: '$ctrl',
+  //       size: size,
+  //       appendTo: parentElem,
+  //       resolve: {
+  //         items: function () {
+  //           return $ctrl.items;
+  //         }
+  //       }
+  //     });
 
-      modalInstance.result.then(function (selectedItem) {
-        $ctrl.selected = selectedItem;
-      }, function () {
-        $log.info('Modal dismissed at: ' + new Date());
-      });
-    };
+  //     modalInstance.result.then(function (selectedItem) {
+  //       $ctrl.selected = selectedItem;
+  //     }, function () {
+  //       $log.info('Modal dismissed at: ' + new Date());
+  //     });
+  //   };
 
-    $ctrl.openComponentModal = function () {
-      var modalInstance = $uibModal.open({
-        animation: $ctrl.animationsEnabled,
-        component: 'modalComponent',
-        resolve: {
-          items: function () {
-            return $ctrl.items;
-          }
-        }
-      });
+  //   $ctrl.openComponentModal = function () {
+  //     var modalInstance = $uibModal.open({
+  //       animation: $ctrl.animationsEnabled,
+  //       component: 'modalComponent',
+  //       resolve: {
+  //         items: function () {
+  //           return $ctrl.items;
+  //         }
+  //       }
+  //     });
 
-      modalInstance.result.then(function (selectedItem) {
-        $ctrl.selected = selectedItem;
-      }, function () {
-        $log.info('modal-component dismissed at: ' + new Date());
-      });
-    };
+  //     modalInstance.result.then(function (selectedItem) {
+  //       $ctrl.selected = selectedItem;
+  //     }, function () {
+  //       $log.info('modal-component dismissed at: ' + new Date());
+  //     });
+  //   };
 
-    $ctrl.openMultipleModals = function () {
-      $uibModal.open({
-        animation: $ctrl.animationsEnabled,
-        ariaLabelledBy: 'modal-title-bottom',
-        ariaDescribedBy: 'modal-body-bottom',
-        templateUrl: 'stackedModal.html',
-        size: 'sm',
-        controller: function($scope) {
-          $scope.name = 'bottom';
-        }
-      });
+  //   $ctrl.openMultipleModals = function () {
+  //     $uibModal.open({
+  //       animation: $ctrl.animationsEnabled,
+  //       ariaLabelledBy: 'modal-title-bottom',
+  //       ariaDescribedBy: 'modal-body-bottom',
+  //       templateUrl: 'stackedModal.html',
+  //       size: 'sm',
+  //       controller: function($scope) {
+  //         $scope.name = 'bottom';
+  //       }
+  //     });
 
-      $uibModal.open({
-        animation: $ctrl.animationsEnabled,
-        ariaLabelledBy: 'modal-title-top',
-        ariaDescribedBy: 'modal-body-top',
-        templateUrl: 'stackedModal.html',
-        size: 'sm',
-        controller: function($scope) {
-          $scope.name = 'top';
-        }
-      });
-    };
+  //     $uibModal.open({
+  //       animation: $ctrl.animationsEnabled,
+  //       ariaLabelledBy: 'modal-title-top',
+  //       ariaDescribedBy: 'modal-body-top',
+  //       templateUrl: 'stackedModal.html',
+  //       size: 'sm',
+  //       controller: function($scope) {
+  //         $scope.name = 'top';
+  //       }
+  //     });
+  //   };
 
-    $ctrl.toggleAnimation = function () {
-      $ctrl.animationsEnabled = !$ctrl.animationsEnabled;
-    } // _______END OF MODAL_______
+  //   $ctrl.toggleAnimation = function () {
+  //     $ctrl.animationsEnabled = !$ctrl.animationsEnabled;
+  //   } // _______END OF MODAL_______
 
-  }) // END OF DOCUMENT.READY
+  // }) // END OF DOCUMENT.READY
 
 })
--- app/controllers/myGigsCtrl.js
+++ app/controllers/myGigsCtrl.js
@@ -1,7 +1,5 @@
 app.controller('myGigsCtrl', function ($scope, gigs) {
 
-  console.log(`firing the myGigs controller`)
-
   $scope.gigs = gigs
 
 })
